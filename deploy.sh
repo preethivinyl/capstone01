c352ad8506504c31764ad8de117dc9fac4b27df7
preethivinyl <74344787+preethivinyl@users.noreply.github.com>
2024-05-10 19:01:45 +0530

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [[ $GIT_BRANCH == "dev" ]];then
+if [[ $GIT_BRANCH == "origin/dev" ]];then
 
 docker login -u preethivinil -p Beautifullife@14
 docker tag devops-build preethivinil/dev:project01
 docker push preethivinil/dev:project01
 
-elif [[ $GIT_BRANCH == "dev" ]];then
+elif [[ $GIT_BRANCH == "origin/dev" ]];then
 
 docker login -u preethivinil -p Beautifullife@14
 docker tag devops-build preethivinil/prod:project01
